79d7bd961883d500b9a0e74a1d9bd517e45ee78f
Johan Sylvan <gtc124@gmail.com>
2018-03-23 12:11:19 +0100

    Changed header to follow Wordpress standard

--- public/themes/iplay/header.php
+++ public/themes/iplay/header.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <?php wp_head(); ?>
-
+<meta charset="<?php bloginfo('charset'); ?>">
+<title><?php wp_title(); ?></title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<?php wp_head(); ?>
 </head>
 
 <body>
