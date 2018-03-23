1fb5bd63feaeeb5781ae8481be2a5faadeabf42e
GabrielAanesen <31622983+GabrielAanesen@users.noreply.github.com>
2018-03-23 11:38:26 +0100

    woops

--- public/themes/iplay/header.php
+++ public/themes/iplay/header.php
@@ -5,5 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <?php wp_head(); ?>
+
 </head>
+
 <body>
+  <header>
+    <nav role="navigation">
+      <?php wp_nav_menu(['theme_location' => 'primary-menu']); ?>
+  </nav>
+  </header>
