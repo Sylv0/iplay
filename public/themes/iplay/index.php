7a5dd5d45e7b25e618ceb4d43c6ab43fdf5101ad
Johan Sylvan <gtc124@gmail.com>
2018-04-09 08:59:20 +0200

    Index page, now with some good content

--- public/themes/iplay/index.php
+++ public/themes/iplay/index.php
@@ -1,12 +1,20 @@
 <?php get_header(); ?>
 
-<?php
-if(have_posts()):
-while (have_posts()) : the_post();
-    the_title();
-    the_content();
-endwhile;
-endif;
-?>
+
+
+<main>
+    <section class="hero">
+        <div class="hero-text">
+            <h3>Visste du att:</h3>
+            <h3>[name] finns på Iplay?</h3>
+            <p>Följ dina favoritatleter på Iplay — Ett socialt nätverk för sportfans och professionella atleter!  Ladda ner appen till din smartphone för att hitta din favoritatlet, din coach eller ditt lag på Iplay!</p>
+            <a href="#" class="store-link google-play"></a>
+            <a href="#" class="store-link app-store"></a>
+        </div>
+        <div class="hero-phone">
+            <p>[image]</p>
+        </div>
+    </section>
+</main>
 
 <?php get_footer(); ?>
