07907a25d7293441e50a512244f15acbc19ccea3
Johan Sylvan <gtc124@gmail.com>
2018-04-10 09:55:54 +0200

    Made random hero, broke CSS kinda

--- public/themes/iplay/index.php
+++ public/themes/iplay/index.php
@@ -1,21 +1,24 @@
 <?php get_header(); ?>
 
 <?php
-    $posts = get_posts(['post_type' => 'hero']);
+if($posts = get_posts(['post_type' => 'hero']))
+    $post = $posts[rand(0, sizeof($posts)-1)];
+else
+    die;
 ?>
 
 <main>
     <section class="hero">
-        <img src="<?php echo get_field('hero_img', $posts[0])['url']; ?>" alt="Player <?php echo $posts[0]->post_title; ?>" class="hero_img">
+        <img src="<?= get_field('hero_img', $post)['url']; ?>" alt="Player <?= $post->post_title; ?>" class="hero_img">
         <div class="hero-text">
             <h3>Visste du att:</h3>
-            <h3><?php echo $posts[0]->post_title ?> finns på Iplay?</h3>
+            <h3><?php echo $post->post_title ?> finns på Iplay?</h3>
             <p>Följ dina favoritatleter på Iplay — Ett socialt nätverk för sportfans och professionella atleter!  Ladda ner appen till din smartphone för att hitta din favoritatlet, din coach eller ditt lag på Iplay!</p>
             <a href="#" class="store-link google-play"></a>
             <a href="#" class="store-link app-store"></a>
         </div>
         <div class="hero-phone">
-            <img src="<?php echo get_field('phone_img', $posts[0]->ID)['url']; ?>" alt="">
+            <img src="<?= get_field('phone_img', $post->ID)['url']; ?>" alt="">
         </div>
     </section>
 </main>
