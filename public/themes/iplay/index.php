ec9073e2e7b04866843acdd61658c675b0dbbea2
GabrielAanesen <31622983+GabrielAanesen@users.noreply.github.com>
2018-04-11 14:46:44 +0200

    Renamed classes

--- public/themes/iplay/index.php
+++ public/themes/iplay/index.php
@@ -9,17 +9,22 @@ else
 
 <main>
     <section class="hero">
-        <img src="<?= get_field('hero_img', $post)['url']; ?>" alt="Player <?= $post->post_title; ?>" class="hero_img">
-        <div class="hero-text">
+        <div class="heroboxcontainer">
+        <div class="hero-text herobox">
             <h3>Visste du att:</h3>
             <h3><?php echo $post->post_title ?> finns på Iplay?</h3>
             <p>Följ dina favoritatleter på Iplay — Ett socialt nätverk för sportfans och professionella atleter!  Ladda ner appen till din smartphone för att hitta din favoritatlet, din coach eller ditt lag på Iplay!</p>
-            <a href="#" class="store-link google-play"></a>
-            <a href="#" class="store-link app-store"></a>
+            <a href="#" class="store-link google-play">Google Play</a>
+            <a href="#" class="store-link app-store">App Store</a>
         </div>
-        <div class="hero-phone">
+        <div class="herobox">
+          <img src="<?= get_field('hero_img', $post)['url']; ?>" alt="Player <?= $post->post_title; ?>" class="hero_img">
+
+        </div>
+        <div class="hero-phone herobox">
             <img src="<?= get_field('phone_img', $post->ID)['url']; ?>" alt="">
         </div>
+      </div>
     </section>
 </main>
 
