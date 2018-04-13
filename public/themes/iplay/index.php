453117ee18fba9a174a4cd3a25e8f6728bf78617
Johan Sylvan <gtc124@gmail.com>
2018-04-13 09:40:15 +0200

    Fixed store links

--- public/themes/iplay/index.php
+++ public/themes/iplay/index.php
@@ -15,8 +15,10 @@ else
             <h3>Visste du att:</h3>
             <h3><?php echo $post->post_title ?> finns på Iplay?</h3>
             <p>Följ dina favoritatleter på Iplay — Ett socialt nätverk för sportfans och professionella atleter!  Ladda ner appen till din smartphone för att hitta din favoritatlet, din coach eller ditt lag på Iplay!</p>
-            <a href="#" class="store-link google-play">Google Play</a>
-            <a href="#" class="store-link app-store">App Store</a>
+            <div>
+                <a href="#" class="store-link google-play">Google Play</a>
+                <a href="#" class="store-link app-store">App Store</a>
+            </div>
         </div>
         <div class="herobox">
           <img src="<?= get_field('hero_img', $post)['url']; ?>" alt="Player <?= $post->post_title; ?>" class="hero_img">
