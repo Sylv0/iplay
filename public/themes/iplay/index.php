ade6569a52884ed5742ebfe7e0c2faf3b01b6ac7
Johan Sylvan <gtc124@gmail.com>
2018-04-13 13:38:31 +0200

    Fixed and broked things

--- public/themes/iplay/index.php
+++ public/themes/iplay/index.php
@@ -17,11 +17,11 @@ else
             <h3><?php echo $post->post_title ?> finns på Iplay?</h3>
             <p>Följ dina favoritatleter på Iplay — Ett socialt nätverk för sportfans och professionella atleter!  Ladda ner appen till din smartphone för att hitta din favoritatlet, din coach eller ditt lag på Iplay!</p>
             <div>
-                <a href="#" class="store-link google-play">Google Play</a>
-                <a href="#" class="store-link app-store">App Store</a>
+                <a href="#" class="store-link google-play">Google Play <i class="fab fa-google-play"></i></a>
+                <a href="#" class="store-link app-store">App Store <i class="fab fa-apple"></i></a>
             </div>
         </div>
-        <div class="herobox">
+        <div class="hero-img herobox">
           <img src="<?= get_field('hero_img', $post)['url']; ?>" alt="Player <?= $post->post_title; ?>" class="hero_img">
         </div>
         <div class="hero-phone herobox">
