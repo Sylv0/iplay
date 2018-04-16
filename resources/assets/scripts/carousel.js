8d5260b60bdd80fc4df6fdbc39e5859fe8773dcf
Johan Sylvan <gtc124@gmail.com>
2018-04-16 16:44:55 +0200

    Removed more comments

--- resources/assets/scripts/carousel.js
+++ resources/assets/scripts/carousel.js
@@ -42,36 +42,3 @@ elements.forEach(element => {
     UpdatePositions(event, parseInt(element.dataset.position));
   });
 });
-
-// elements.forEach(element =>
-//   element.addEventListener("click", event => {
-//     const position = parseInt(element.dataset.position);
-
-//     if (position === 1) {
-//       elements.forEach(child => {
-//         var parseDataset = parseInt(child.dataset.position);
-//         if (parseDataset === 6) {
-//           child.dataset.position = 1;
-//         } else {
-//           child.dataset.position = parseInt(child.dataset.position) + 1;
-//         }
-//       });
-//     }
-
-//     if (position === 3) {
-//       elements.forEach(child => {
-//         var parseDataset = parseInt(child.dataset.position);
-//         if (parseDataset === 1) {
-//           child.dataset.position = 6;
-//         } else {
-//           child.dataset.position -= 1;
-//         }
-//       });
-//     }
-//     document.querySelector(".active-user").innerHTML = document.querySelector(
-//       'img[data-position="2"]'
-//     ).dataset["name"];
-//   })
-// );
-
-window.onload = () => {};
