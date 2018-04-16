1297a9bc4b34c87ce3a2408af81cf9ce17d4f0c6
GabrielAanesen <31622983+GabrielAanesen@users.noreply.github.com>
2018-04-16 13:17:53 +0200

    further work on carousel

--- public/themes/iplay/index.php
+++ public/themes/iplay/index.php
@@ -39,7 +39,9 @@ else
           <h2>Följ dina favoritatleter på Iplay redan idag!</h2><p>Ett socialt nätverk för sportfans och professionella atleter! Ladda ner appen till din smartphone för att hitta din favoritatlet, din coach eller ditt lag på Iplay!</p>
         </div>
         <div class="users2">
-          <h2>Iplay för:</h2>
+          <div class="">
+            <h2>Iplay för:</h2>
+          </div>
           <div class="hero-image-sub-carousel">
             <img class="hero-image-badge login-img" data-position="1" src="themes/iplay/assets/images/swipegubbe.svg" alt="">
             <img class="hero-image-badge profile-img" data-position="2" src="themes/iplay/assets/images/swipegubbe.svg" alt="">
