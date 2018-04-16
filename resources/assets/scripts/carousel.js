2d180292e10d8b11e8ef67d1d0955fe2b4ba605b
Johan Sylvan <gtc124@gmail.com>
2018-04-16 16:17:00 +0200

    IT'S ALIVE!!!

--- resources/assets/scripts/carousel.js
+++ resources/assets/scripts/carousel.js
@@ -1,24 +1,77 @@
 // HERO CAROUSEL
-const section = document.querySelector('.hero-image-sub-carousel');
-const elements = [...document.querySelectorAll('img')];
+const section = document.querySelector(".hero-image-sub-carousel");
+const elements = [...document.querySelectorAll("img")];
 
-elements.forEach(element => element.addEventListener('click', event => {
-  const position = parseInt(element.dataset.position);
-
+const UpdatePositions = (event, position) => {
   if (position === 1) {
     elements.forEach(child => {
       var parseDataset = parseInt(child.dataset.position);
-      if (parseDataset === 6) { child.dataset.position = 1; }
-      else { child.dataset.position = parseInt(child.dataset.position) + 1; }
+      if (parseDataset === 6) {
+        child.dataset.position = 1;
+      } else {
+        child.dataset.position = parseInt(child.dataset.position) + 1;
+      }
     });
   }
 
   if (position === 3) {
     elements.forEach(child => {
       var parseDataset = parseInt(child.dataset.position);
-      if (parseDataset === 1) { child.dataset.position = 6; }
-      else { child.dataset.position -= 1; }
+      if (parseDataset === 1) {
+        child.dataset.position = 6;
+      } else {
+        child.dataset.position -= 1;
+      }
     });
   }
-  document.querySelector('.active-user').innerHTML = document.querySelector('img[data-position="2"]').dataset['name'];
-}));
+  document.querySelector(".active-user").innerHTML = document.querySelector(
+    'img[data-position="2"]'
+  ).dataset["name"];
+};
+
+setTimeout(() => {
+  document.querySelectorAll('svg[data-position]').forEach(e => {
+    e.addEventListener("click", event => {
+      UpdatePositions(event, parseInt(e.dataset.position));
+    });
+  });
+}, 1000);
+
+elements.forEach(element => {
+  element.addEventListener("click", event => {
+    UpdatePositions(event, parseInt(element.dataset.position));
+  });
+});
+
+// elements.forEach(element =>
+//   element.addEventListener("click", event => {
+//     const position = parseInt(element.dataset.position);
+
+//     if (position === 1) {
+//       elements.forEach(child => {
+//         var parseDataset = parseInt(child.dataset.position);
+//         if (parseDataset === 6) {
+//           child.dataset.position = 1;
+//         } else {
+//           child.dataset.position = parseInt(child.dataset.position) + 1;
+//         }
+//       });
+//     }
+
+//     if (position === 3) {
+//       elements.forEach(child => {
+//         var parseDataset = parseInt(child.dataset.position);
+//         if (parseDataset === 1) {
+//           child.dataset.position = 6;
+//         } else {
+//           child.dataset.position -= 1;
+//         }
+//       });
+//     }
+//     document.querySelector(".active-user").innerHTML = document.querySelector(
+//       'img[data-position="2"]'
+//     ).dataset["name"];
+//   })
+// );
+
+window.onload = () => {};
